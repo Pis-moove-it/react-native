134b321820d3cad8f6ba301e73114c3b1e499972
Joaguirrem <joaquin.aguirre@fing.edu.uy>
2018-09-20 16:14:47 -0300

    Fix indentation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,22 +17,22 @@ import { errorsSelector } from '../../selectors/ErrorSelector';
 
 
 class Login extends Component {
-	
-	constructor() {
-		super();
-		this.state = {
-			identifier: null,
-			username: null,
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      identifier: null,
+      username: null,
+      loading: false,
+    };
+  }
 
-	static navigatorStyle = {
-		navBarHidden: true,
-	};
+  static navigatorStyle = {
+    navBarHidden: true,
+  };
 
-	componentDidMount() {
-		this.props.fetchData();
-	}
+  componentDidMount() {
+    this.props.fetchData();
+  }
 
   static getDerivedStateFromProps(nextProps) {
     if (nextProps.user !== null) {
@@ -41,83 +41,83 @@ class Login extends Component {
     return null;
   }
 
-	usernameChanged = (itemValue, itemIndex) => this.setState({identifier: itemIndex, username: itemValue});
+  usernameChanged = (itemValue, itemIndex) => this.setState({identifier: itemIndex, username: itemValue});
 
-	login = () => {
-		this.setState({loading: true});
-		this.props.login(this.state.identifier, this.state.username);
-	};
+  login = () => {
+    this.setState({loading: true});
+    this.props.login(this.state.identifier, this.state.username);
+  };
 
-	getUsers() {
-		var usersData = [];
-		usersData.push(<Picker.Item key={999} label={strings.user} value={null} />);
-		this.props.users.data.map((user, identifier) => {
-			usersData.push( <Picker.Item key={identifier} label={`${user.name} ${user.surname}`} value={`${user.name} ${user.surname}`} />)
-		});
-		return usersData;
-	}
+  getUsers() {
+    var usersData = [];
+    usersData.push(<Picker.Item key={999} label={strings.user} value={null} />);
+    this.props.users.data.map((user, identifier) => {
+      usersData.push( <Picker.Item key={identifier} label={`${user.name} ${user.surname}`} value={`${user.name} ${user.surname}`} />)
+    });
+    return usersData;
+  }
 
-	render() {
-		const {isLoading, errors} = this.props;
-		let loading = this.state.loading;
-		return (
-			<View style={styles.container}>
-				<View style={styles.topContainer}>
-					<Image source={require('./../../assets/images/Logo03.png')} style={styles.logo} />
-				</View>
-				<View style={[styles.bottomContainer, ShadowStyles.shadow]}>
-					<View style={styles.pickerContainer}>
-						<Image source={require('./../../assets/ic_user/ic_user128.png')} style={styles.icon} />
-						<Picker
-							selectedValue={this.state.username}
-							style={styles.picker}
-							mode='dialog'
-							onValueChange={this.usernameChanged}
-						>
-							{this.getUsers()}
-						</Picker>
-					</View>
-					<ErrorView errors={errors} />
-					{loading && errors.length < 1 ?
-						<View style={styles.activityIndicator}>
-							<ActivityIndicator size="large" color={Colors.primary} />
-						</View>
-					:
-						<Button
-							style={styles.button}
-							textStyle={styles.textButton}
-							onPress={this.state.username !== null ? this.login : null}
-							title={strings.login}
-						/>
-					}
-				</View>
-			</View>
-		);
-	}
+  render() {
+    const {isLoading, errors} = this.props;
+    let loading = this.state.loading;
+    return (
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <Image source={require('./../../assets/images/Logo03.png')} style={styles.logo} />
+        </View>
+        <View style={[styles.bottomContainer, ShadowStyles.shadow]}>
+          <View style={styles.pickerContainer}>
+            <Image source={require('./../../assets/ic_user/ic_user128.png')} style={styles.icon} />
+            <Picker
+              selectedValue={this.state.username}
+              style={styles.picker}
+              mode='dialog'
+              onValueChange={this.usernameChanged}
+            >
+              {this.getUsers()}
+            </Picker>
+          </View>
+          <ErrorView errors={errors} />
+          {loading && errors.length < 1 ?
+            <View style={styles.activityIndicator}>
+              <ActivityIndicator size="large" color={Colors.primary} />
+            </View>
+          :
+            <Button
+              style={styles.button}
+              textStyle={styles.textButton}
+              onPress={this.state.username !== null ? this.login : null}
+              title={strings.login}
+            />
+          }
+        </View>
+      </View>
+    );
+  }
 }
 
 Login.propTypes = {
-	login: PropTypes.func.isRequired,
-	user: PropTypes.object,
-	isLoading: PropTypes.bool.isRequired,
-	errors: PropTypes.array,
+  login: PropTypes.func.isRequired,
+  user: PropTypes.object,
+  isLoading: PropTypes.bool.isRequired,
+  errors: PropTypes.array,
 };
 
 Login.defaultProps = {
-	user: null,
-	errors: [],
+  user: null,
+  errors: [],
 };
 
 const mapStateToProps = state => ({
-	user: getUser(state),
-	isLoading: loadingSelector([actionTypes.LOGIN])(state),
-	errors: errorsSelector([actionTypes.LOGIN])(state),
-	users: state.data,
+  user: getUser(state),
+  isLoading: loadingSelector([actionTypes.LOGIN])(state),
+  errors: errorsSelector([actionTypes.LOGIN])(state),
+  users: state.data,
 });
 
 const mapDispatchToProps = dispatch => ({
-	login: (identifier, username) => dispatch(login(identifier, username)),
-	fetchData: () => dispatch(fetchData()),
+  login: (identifier, username) => dispatch(login(identifier, username)),
+  fetchData: () => dispatch(fetchData()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
