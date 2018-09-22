f79fdcd36a576cdbaf9390394b96bdbaff409271
anitanigma <ana.machado@fing.edu.uy>
2018-09-22 19:36:55 -0300

    Remove outdated http controller file

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-import httpClient from './HttpClient';
+
 
 class UserController {
 	constructor() {
@@ -6,23 +6,6 @@ class UserController {
 	}
 
 	login = async (identifier, username) => {
-		// Real implementation of a login request using the HttpClient
-		/* try {
-			const result = await httpClient.post({
-				url: `${this.basePath}/session`,
-				method: 'POST',
-				data: {
-					email,
-					password,
-				},
-			});
-			return result.data.user;
-			// Data is the object exposes by axios for the response json
-		} catch (error) {
-			return error;
-		}
-		*/
-		// This is a mocked example to simulate api behavior
 		return new Promise((resolve, reject) => {
 			if (identifier !== null && username !== null) {
 				setTimeout(
