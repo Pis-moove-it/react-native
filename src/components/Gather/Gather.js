0f7469d7457beeaa44f88e6b61eb4b91e77c44e9
Daniel Susviela <d.susviela@gmail.com>
2018-09-27 19:52:08 -0300

    map-integrated-missing-welcome-page

--- src/components/Gather/Gather.js
+++ src/components/Gather/Gather.js
@@ -1,56 +1,3 @@
-/*import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import TextStyles from '../../helpers/TextStyles';
-import strings from '../../localization';
-import getUser from '../../selectors/UserSelector';
-import getRole from '../../selectors/RoleSelector';
-import styles from './styles';
-
-class Gather extends Component {
-  static navigatorStyle = {
-    navBarHidden: true,
-  };
-
-  render() {
-    const { user } = this.props;
-    const { role } = this.props;
-    return (
-      <View style={styles.container}>
-        <Text style={TextStyles.lightTitle}>{strings.gatherTitle}</Text>
-        <Text>{`${strings.homeMessage} ${user && user.name}`}</Text>
-        <Text style={TextStyles.lightTitle}>{`${role}`}</Text>
-      </View>
-    );
-  }
-}
-
-Gather.propTypes = {
-  user: PropTypes.object,
-  role: PropTypes.string,
-};
-
-Gather.defaultProps = {
-  user: null,
-  role: null,
-};
-
-const mapStateToProps = state => ({
-  user: getUser(state),
-  role: getRole(state),
-});
-
-const mapDispatchToProps = () => ({});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Gather);
-*/
-
-
-
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
@@ -60,7 +7,7 @@ Mapbox.setAccessToken('pk.eyJ1IjoicXFtZWxvIiwiYSI6ImNqbWlhOXh2eDAwMHMzcm1tNW1veD
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={stylesGather.container}>
         <Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
             zoomLevel={15}
@@ -72,7 +19,7 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
+const stylesGather = StyleSheet.create({
   container: {
     flex: 1,
   },
