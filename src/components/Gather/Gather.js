ee29596ba249ae386aad1fdfd0b97580ceecfd2d
Daniel Susviela <d.susviela@gmail.com>
2018-09-30 16:55:49 -0300

    reviewed-code-from-pr

--- src/components/Gather/Gather.js
+++ src/components/Gather/Gather.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
+import stylesGather from './styles';
 
 Mapbox.setAccessToken('pk.eyJ1IjoicXFtZWxvIiwiYSI6ImNqbWlhOXh2eDAwMHMzcm1tNW1veDNmODYifQ.vOmFAXiikWFJKh3DpmsPDA');
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
   render() {
     return (
-      <View style={stylesGather.container}>
+      <View style={stylesGather.mapContainer}>
         <Mapbox.MapView
-            styleURL={Mapbox.StyleURL.Street}
-            zoomLevel={15}
-            centerCoordinate={[11.256, 43.770]}
-            style={styles.container}>
-        </Mapbox.MapView>
+          styleURL={Mapbox.StyleURL.Street}
+          zoomLevel={15}
+          centerCoordinate={[11.256, 43.77]}
+          style={stylesGather.mapContainer}
+        />
       </View>
     );
   }
 }
-
-const stylesGather = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
--- src/components/Gather/styles.js
+++ src/components/Gather/styles.js
@@ -1,13 +1,16 @@
 import { StyleSheet } from 'react-native';
 import Colors from '../../helpers/Colors';
 
-const styles = StyleSheet.create({
+const stylesGather = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.primary,
   },
+  mapContainer: {
+    flex: 1,
+  },
 });
 
-export default styles;
+export default stylesGather;
