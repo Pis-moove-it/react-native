2366f70fa4ad27f5575aecfe7ff9066416806b99
Daniel Susviela <d.susviela@gmail.com>
2018-10-01 18:02:54 -0300

    overlay-done

--- src/components/Gather/Gather.js
+++ src/components/Gather/Gather.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 import stylesGather from './styles';
+import GatherOverlay from './GatherOverlay';
+
 
 Mapbox.setAccessToken('pk.eyJ1IjoicXFtZWxvIiwiYSI6ImNqbWlhOXh2eDAwMHMzcm1tNW1veDNmODYifQ.vOmFAXiikWFJKh3DpmsPDA');
 
@@ -10,10 +12,17 @@ export default class App extends Component {
     super(props);
     this.props = props;
   }
+  state = {
+    isModalVisible: false,
+  };
+
+  toggleModal = () =>
+    this.setState({ isModalVisible: !this.state.isModalVisible });
 
   render() {
     return (
       <View style={stylesGather.mapContainer}>
+        <GatherOverlay />
         <Mapbox.MapView
           styleURL={Mapbox.StyleURL.Street}
           zoomLevel={15}
