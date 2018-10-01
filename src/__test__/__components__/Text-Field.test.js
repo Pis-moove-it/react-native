3e7c98f29ec09f64809055fece17e528e73aad14
Ana Machado <ana@wyeworks.com>
2018-10-01 17:21:31 -0300

    Add test info

--- src/__test__/__components__/Text-Field.test.js
+++ src/__test__/__components__/Text-Field.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TextField from '../../components/common/TextField';
 
-test('renders correctly', () => {
+test('Text Field renders correctly', () => {
   const tree = renderer.create(<TextField />).toJSON;
   expect(tree).toMatchSnapshot();
 });
