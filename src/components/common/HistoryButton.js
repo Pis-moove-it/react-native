0f5ce9b12bc1a9bfbb7eb7494768d9e691583f08
Ana Machado <ana@wyeworks.com>
2018-10-02 17:10:26 -0300

    Add custom HistoryButton

---
+++ src/components/common/HistoryButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity, StyleSheet, Image, View } from 'react-native';
+import Colors from '../../helpers/Colors';
+import HistorialIcon from '../../assets/images/HistorialIcon.png';
+import strings from '../../localization/Localization';
+
+const styles = StyleSheet.create({
+  touchable: {
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    margin: 6,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: Colors.primary,
+  },
+});
+
+const HistoryButton = () => (
+  <TouchableOpacity>
+    <View style={styles.touchable}>
+      <Image style={styles.icon} source={HistorialIcon} />
+      <Text style={styles.text}>{strings.history}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+export default HistoryButton;
