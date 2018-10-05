d6963b3bee3e73fdcd6e715a48dbac4d3a361923
Sebastian Guerrero <chebi97@gmail.com>
2018-10-05 17:51:38 -0300

    Fixed unnecessary white line

--- src/components/Bale/TabletBale.js
+++ src/components/Bale/TabletBale.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Image, Text } from 'react-native';
 import FardoIcon from '../../assets/images/BaleIcon.png';
-
 import Button from '../common/Button';
 import strings from '../../localization';
 import styles from './styles';
