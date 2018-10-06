32ce7840ff69505a1cc04e348f80fb6c3f91760e
Ana Machado <ana@wyeworks.com>
2018-10-06 19:58:14 -0300

    Delete deprecated test

--- src/__test__/__components__/Text-Field.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import TextField from '../../components/common/TextField';
-
-test('Text Field renders correctly', () => {
-  const tree = renderer.create(<TextField />).toJSON;
-  expect(tree).toMatchSnapshot();
-});
