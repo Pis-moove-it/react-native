413a9b349e60d28fb0a41a0b1ca6703b055c13bb
Ana Machado <ana@wyeworks.com>
2018-10-10 20:01:09 -0300

    Wip

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const staging = 'http://34.213.11.120/'; // Change 1 for {UserOrganization} with new Login
+const staging = 'http://34.213.11.120/';
 
 export const getUsersApi = organizationId =>
   axios.get(`${staging}organizations/${organizationId}/users`);
