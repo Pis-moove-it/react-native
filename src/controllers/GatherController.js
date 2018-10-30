20f936f569e6ac7cf6175f88d834b878f9d79108
Ana Machado <anitanigma@gmail.com>
2018-10-30 20:30:26 -0300

    Add method to add pocket to collection point
    Changed path to routesPath to add the rest of the paths with their respective names

--- src/controllers/GatherController.js
+++ src/controllers/GatherController.js
@@ -4,14 +4,16 @@ import basePath, { Network } from './BaseController';
 
 class GatherController {
   constructor() {
-    this.path = 'routes/';
+    this.routeID = null;
+    this.routesPath = 'routes/';
+    this.collectionsPath = `routes/${this.routeID}/collections`;
   }
 
   startCollection = async token =>
     new Promise((resolve, reject) => {
       axios
         .post(
-          `${basePath}${this.path}`,
+          `${basePath}${this.routesPath}`,
           {},
           {
             headers: {
@@ -24,6 +26,32 @@ class GatherController {
           resolve({
             identifier: response.data.id,
           });
+        })
+        .catch((error) => {
+          if (error.message.includes(Network)) reject(new Error(strings.errorNetwork));
+          else reject(new Error(strings.errorUser));
+        });
+    });
+
+  addPocketToCollection = async (token, routeId, collectionId, pocket) =>
+    new Promise((resolve, reject) => {
+      this.routeID = routeId;
+      axios
+        .post(
+          `${basePath}${this.collectionsPath}`,
+          {
+            collection_point_id: collectionId,
+            pockets_attributes: [{ serial_number: `${pocket.serial_number}` }],
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              ApiKey: `${token}`,
+            },
+          },
+        )
+        .then(() => {
+          resolve({});
         })
         .catch((error) => {
           if (error.message.includes(Network)) reject(new Error(strings.errorNetwork));
