774078be8d8bc6e5fc498b466a3d95342e103c76
Joaguirrem <joaquin.aguirre@fing.edu.uy>
2018-10-31 21:19:10 -0300

    delete console logs

--- src/helpers/Permissions.js
+++ src/helpers/Permissions.js
@@ -2,20 +2,10 @@ import { PermissionsAndroid } from 'react-native';
 
 async function requestLocationPermission() {
   try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Reciclando',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the location');
-    } else {
-      console.log('location permission denied');
-    }
-  } catch (err) {
-    console.warn(err);
-  }
+    await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+      title: 'Reciclando',
+    });
+  } catch (err) {}
 }
 
 export default requestLocationPermission;
